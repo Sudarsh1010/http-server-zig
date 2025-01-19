9a4cdce7a0a46820d5505a9dcc6b286abc1b6276
Sudarsh1010 <sudarshjain1010@gmail.com>
2025-01-19 19:30:20 +0530

    read request obj

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,18 @@
 const std = @import("std");
 const SocketConf = @import("socket.zig").Socket;
+const request = @import("request.zig");
+const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
-    _ = try SocketConf.init();
+    const socket = try SocketConf.init();
+    var server = try socket.address.listen(.{});
+    const connection = try server.accept();
+
+    var buffer: [1000]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+
+    _ = try request.readRequest(connection, &buffer);
+    try stdout.print("request: {s}\n", .{buffer});
 }
